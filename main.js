56680ec605560c40fe302a20a3f81311616764d4
Wenjie Xu <garreyxu@gmail.com>
2020-09-25 00:12:14 -0700

    Lesson 5

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'http://www.google.com',
             inventory: 0,
-            onSale: false
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
+            sizes: ['S', 'M', 'L', 'XL']
         }
     }
 })
