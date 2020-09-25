61887360f936f7e3ea5a6a3fef79d958ede20486
Wenjie Xu <garreyxu@gmail.com>
2020-09-24 23:53:45 -0700

    Lesson 2-4

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            descritopn: "description placeholder"
+            descritopn: 'description placeholder',
+            image: './assets/images/socks_green.jpg',
+            url: 'http://www.google.com',
+            inventory: 0,
+            onSale: false
         }
     }
 })
